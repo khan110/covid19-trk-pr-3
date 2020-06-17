3b913e33f60f13845a779ba4ca97594ec551322c
Umair ali khan <umair_kh90@yahoo.com>
2020-06-17 17:08:14 -0500

    Update CSS module files fro components to make it responsive

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { CountryPicker } from "./components/Country picker/CountryPicker";
 import styles from "./App.module.css";
 import { fetchdata } from "./API/index";
 
+import covid from './images/covidm.jpg';
+
 class App extends React.Component {
   state = {
     data: {},
@@ -25,6 +27,7 @@ class App extends React.Component {
     const { data, country } = this.state;
     return (
       <div className={styles.container}>
+        <img className={styles.image} src={covid} alt="Covid-19" />
         <Cards data={data} />
         <CountryPicker handleCountryChange={this.handleCountryChange} />
         <Charts data={data} country={country}  />
